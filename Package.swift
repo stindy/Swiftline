71188fd23191838aecb0d7153182e996de537f88
azoom-nguyen-chi-thanh <nguyen.chi.thanh@azoom.jp>
2023-11-29 20:16:01 +0700

    :+1: Change folder path

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Swiftline"
+    name: "Swiftline",
+    products: [
+        .library(
+            name: "Swiftline",
+            targets: ["Swiftline"]),
+    ],
+    targets: [
+        .target(
+            name: "Swiftline",
+            path: "Sources"
+        ),
+    ]
 )
